75046460a58f85622d6d87d32c85d4cf2db5562b
SQ9NJE <github@sq9nje.pl>
2017-08-27 21:15:55 +0200

    Docstrings & some cleanup

--- xtal.py
+++ xtal.py
@@ -4,24 +4,53 @@ from openpyxl.workbook import Workbook
 from openpyxl.styles import Font
 
 def print_banner():
+    """Prints out a nice banner.
+    
+    """
     print('##################################')
     print('         Rigol DSA815-TG')
     print('    XTAL Parameter Measurement')
     print('##################################')
     print()
 
-def print_idn(instrument):
+def get_idn(instrument):
+    """Querries the instrument for its ID string using the *IDN? SCPI command.
+
+    Args:
+        param1: VISA instrument object
+    Returns:
+        string: Instrument's ID string
+    
+    """
     idn = instrument.query('*IDN?')
-    print('Device ID: {}\n'.format(idn))
+    return idn
 
 def check_tg(instrument):
-    # Check if tracking generator is enabled
+    """ Checks if tracking generator is enabled.
+
+    Args:
+        param1: VISA instrument object
+    Returns:
+        bool: True if TG is enabled
+
+    """
     if int(instrument.query(':OUTPUT:STATE?').strip('\n\r')) == 0:
-        print('!!! Tracking generator not enabled !!!')
-        print('Please refer to the usage manual for instructions on how to setup the instrument')
-        sys.exit(-1)
+        return False
+    else:
+        return True
 
 def setup_markers(instrument):
+    """Enables trace markers and frequency display.
+    Marker 1 features:
+        - Track peak
+        - -3dB bandwidth
+    Frequency display:
+        - 1Hz resolution
+
+    Args:
+        param1: VISA instrument object
+
+    """
     # Enable marker 1
     instrument.write('calc:marker1:state 1')
     # Marker 1 track peak
@@ -36,7 +65,6 @@ def setup_markers(instrument):
     instrument.write('calc:marker:fcount:resolution 1')
 
 
-
 if __name__ == '__main__':
     print_banner()
     # Connect to first Rigol DSA device using VISA
@@ -49,10 +77,16 @@ if __name__ == '__main__':
     print('Connecting to device...')
     rigol = rm.open_resource(dsa[0])
     rigol.timeout = 10000
+    
     # Print device ID
-    print_idn(rigol)
+    print('Device ID: {}\n'.format(get_idn(rigol)))
+    
     # Check if tracking generator is enabled
-    check_tg(rigol)
+    if not check_tg(rigol):
+        print('!!! Tracking generator not enabled !!!')
+        print('Please refer to the usage manual for instructions on how to setup the instrument')
+        sys.exit(-1)
+    
     # Configure marker functions
     setup_markers(rigol)
 
@@ -77,7 +111,7 @@ if __name__ == '__main__':
     ws_row = 1
 
     # Measure next oscillator
-    while raw_input('Measure XTAL? [Y/n]: ') != 'n':
+    while raw_input('Measure XTAL? [Y/n]: ').lower() != 'n':
         ws_row = ws_row + 1
 
         fc = int(rigol.query('calc:marker:fcount:x?').strip('\n\r'))
